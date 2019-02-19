496e13aec017be879159f65c3f9f9f071fbfbc8c
donggyukim <donggyukim@donggyukims-MacBook-Air.local>
2019-02-19 14:12:30 +0900

    Initial Commit

---
+++ whatthe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whatthe
+//
+//  Created by donggyukim on 19/02/2019.
+//  Copyright © 2019 donggyukim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
